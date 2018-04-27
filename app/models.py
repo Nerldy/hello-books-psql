81ef5b4e720a89c9c49f3feaf30d9f8dc8b726bf
Nerldy_ <arnodhis@gmail.com>
2018-04-27 16:25:31 +0300

    add is_logged in property add Author class

--- app/models.py
+++ app/models.py
@@ -11,8 +11,9 @@ class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(70), index=True, nullable=False, unique=True)
 	username = db.Column(db.String(70), index=True, nullable=False, unique=True)
-	password_hash = db.Column(db.String(140))
+	password_hash = db.Column(db.String(140), nullable=False)
 	is_admin = db.Column(db.Boolean, default=False)
+	is_logged = db.Column(db.Boolean, default=False)
 	join_date = db.Column(db.DateTime, default=db.func.current_timestamp())
 	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
@@ -46,10 +47,38 @@ def load_user(user_id):
 
 
 class Book(db.Model):
+	"""represents a book"""
 	__tablename__ = "books"
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(160), nullable=False)
 	isbn = db.Column(db.String(20), nullable=False, unique=True)
+	synopsis = db.Column(db.String(200), nullable=False)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+	authors = db.relationship('Author', secondary="books_authors", backref='books', lazy='dynamic')
 
 	def __repr__(self):
 		return f'<Book: {self.title}>'
+
+
+class Author(db.Model):
+	"""represents the authors"""
+
+	__tablename__ = 'authors'
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(30), nullable=False)
+	middle_name = db.Column(db.String(30), nullable=True)
+	last_name = db.Column(db.String(30), nullable=False)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+	def __repr__(self):
+		return f"<Author: {self.first_name} {self.last_name}>"
+
+
+db.Table(
+	'books_authors',
+	db.Column('book_id', db.Integer, db.ForeignKey('books.id')),
+	db.Column('author_id', db.Integer, db.ForeignKey('authors.id'))
+)
