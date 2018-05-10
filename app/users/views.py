63a201f7bc0ac241b52c01b51539e90375f6dd08
Nerldy_ <arnodhis@gmail.com>
2018-05-10 16:22:36 +0300

    add comments

--- app/users/views.py
+++ app/users/views.py
@@ -8,46 +8,41 @@ from ..models import BookList, BorrowedBook
 @users.route('/books/<int:id>', methods=['POST'])
 @login_required
 def api_users_borrow_book(id):
-	# find book id
-	book = BookList.query.filter(BookList.id == id).first()
-	user_id = current_user.get_id()
-	borrow_book = BorrowedBook()
+	book = BookList.query.filter(BookList.id == id).first()  # find book with same id in the books list table
+	user_id = current_user.get_id()  # current user id
+	borrow_book = BorrowedBook()  # instance borrow book class
 
 	# if book exists:
 	if not book:
 		abort(404)
 
-	# if book.user_id == user_id:
-	# 	return jsonify({'message': "you currently have this book borrowed"})
-
-	if book.is_borrowed:
+	if book.is_borrowed:  # check if book is borrowed
 		return jsonify({'message': f"BookList with ID {book.id} is not currently available."})
 
-	borrow_book.user_id = user_id
-	borrow_book.book_id = book.id
-	book.is_borrowed = True
+	borrow_book.user_id = user_id  # create a user id property
+	borrow_book.book_id = book.id  # create a book id property
+	book.is_borrowed = True  # make book borrow property status true
 
-	db.session.add(borrow_book)
-
-	db.session.commit()
 	# add it to the borrowing table
+	db.session.add(borrow_book)
+	db.session.commit()
 	return jsonify({"book_borrowed": f"{book.title}"})
 
 
 @users.route('/books/<int:id>', methods=['PUT'])
 @login_required
 def api_return_book(id):
-	book = BorrowedBook.query.filter(BorrowedBook.book_id == id).first()
-	main_book = BookList.query.filter(BookList.id == id).first()
-	user_id = current_user.get_id()
+	book = BorrowedBook.query.filter(BorrowedBook.book_id == id).first()  # find the book in the borrow table
+	main_book = BookList.query.filter(BookList.id == id).first()  # find the sam book id in the books list table
+	user_id = current_user.get_id()  # current user id
 
 	# if book exists:
 	if not book:
 		abort(404)
 
-	if main_book.is_borrowed:
-		main_book.is_borrowed = False
-		book.return_date = db.func.current_timestamp()
+	if main_book.is_borrowed:  # check if book is borrowed
+		main_book.is_borrowed = False  # make it false
+		book.return_date = db.func.current_timestamp()  # add current time stamp of when book is being return
 		db.session.commit()
 
 		return jsonify({"message": f'book with ID {book.book_id} has been returned'})
@@ -58,22 +53,28 @@ def api_return_book(id):
 @users.route('/books')
 @login_required
 def api_books_not_returned():
-	req_data = request.args.get('returned', None)
+	req_data = request.args.get('returned', None)  # confirm if returned property exists in arguments
+
+	# find user id in the borrow books table and confirm if it match current user
 	user_borrowed_books = BorrowedBook.query.filter(BorrowedBook.user_id == current_user.get_id())
 
-	if req_data:
-		books_not_returned = []
-		book = BookList.query.all()
+	if req_data:  # enter this block if returned property does exist in arguments
+		books_not_returned = []  # empty books returned list
+		book = BookList.query.all()  # get all books in the book list table
 
-		if req_data.lower() == 'false':
-			for book_borrowed in book:
+		if req_data.lower() == 'false':  # make the value of the returned proprty into small caps
 
-				if book_borrowed.is_borrowed:
-					book_obj = {
-						'title': book_borrowed.title
+			for book_borrowed in book:  # loop through the books list
+				if book_borrowed.is_borrowed:  # check status of the book if it's borrowed
+					book_obj = { # create the book object
+						'id': book_borrowed.id,
+						'title': book_borrowed.title,
+						'isbn': book_borrowed.isbn,
+						'synopsis': book_borrowed.synopsis,
+						'date_borrowed': book_borrowed.borrow_date
 					}
 
-					books_not_returned.append(book_obj)
+					books_not_returned.append(book_obj) # add book object to the not returned list
 
 			return jsonify({'books yet to be returned': books_not_returned})
 		abort(404)
