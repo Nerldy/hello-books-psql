07fd3ae30e4fc32552cd2e1fa340a4e65c9d2964
Nerldy_ <arnodhis@gmail.com>
2018-05-10 11:31:26 +0300

    correct typo in available
    update borrow book function

--- app/users/views.py
+++ app/users/views.py
@@ -21,12 +21,11 @@ def api_users_borrow_book(id):
 	# 	return jsonify({'message': "you currently have this book borrowed"})
 
 	if book.is_borrowed:
-		return jsonify({'message': f"BookList with ID {book.id} is not currently availablb."})
+		return jsonify({'message': f"BookList with ID {book.id} is not currently available."})
 
 	borrow_book.user_id = user_id
 	borrow_book.book_id = book.id
 	book.is_borrowed = True
-	borrow_book.is_returned = False
 
 	db.session.add(borrow_book)
 
@@ -48,7 +47,6 @@ def api_return_book(id):
 
 	if main_book.is_borrowed:
 		main_book.is_borrowed = False
-		book.is_returned = True
 		book.return_date = db.func.current_timestamp()
 		db.session.commit()
 
@@ -62,47 +60,72 @@ def api_return_book(id):
 def api_books_not_returned():
 	req_data = request.args.get('returned', None)
 	user_borrowed_books = BorrowedBook.query.filter(BorrowedBook.user_id == current_user.get_id())
-	books_returned = user_borrowed_books.filter(BorrowedBook.is_returned)
-	books_not_returned = user_borrowed_books.filter(BorrowedBook.is_returned == False)
-	books_history = user_borrowed_books.filter(BorrowedBook.return_date)
-	available_books = BookList.query
 
 	if req_data:
-		all_books = []
+		books_not_returned = []
+		book = BookList.query.all()
+
 		if req_data.lower() == 'false':
+			for book_borrowed in book:
 
-			for b in available_books:
-				for e in books_not_returned:
-					if e.book_id == b.id:
-						make_book = {
-							'book_id': b.id,
-							'title': b.title,
-							'isbn': b.isbn,
-							'synopsis': b.synopsis,
-							'date_borrowed': e.borrow_date
-						}
-						all_books.append(make_book)
-			return jsonify({"Books not yet returned": all_books})
+				if book_borrowed.is_borrowed:
+					book_obj = {
+						'title': book_borrowed.title
+					}
 
+					books_not_returned.append(book_obj)
+
+			return jsonify({'books yet to be returned': books_not_returned})
 		abort(404)
 
-	if books_returned is not None:
-		all_books = []
-		for b in available_books:
-			for e in books_returned:
-				if e.book_id == b.id:
-					make_book = {
-						'book_id': b.id,
-						'title': b.title,
-						'isbn': b.isbn,
-						'synopsis': b.synopsis,
-						'date_borrowed': e.borrow_date,
-						'date_returned': e.return_date
-					}
-					all_books.append(make_book)
-		return jsonify({"history of borrowed books": all_books})
+	if user_borrowed_books is not None:
+		all_borrowed_books = []
 
+		for book_borrowed in user_borrowed_books:
+			book = BookList.query.filter(BookList.id == book_borrowed.book_id).first()
 
+			if book.is_borrowed is False:
+				book_obj = {
+					'title': book.title,
+					'date_borrowed': book_borrowed.borrow_date,
+					'date_returned': book_borrowed.return_date
+				}
 
+				all_borrowed_books.append(book_obj)
 
+		return jsonify({'history_of_books_borrowed': all_borrowed_books})
 
+# if req_data:
+# 	all_books = []
+# 	if req_data.lower() == 'false':
+#
+# 		for b in user_borrowed_books:
+# 			for e in books_not_returned:
+# 				if e.book_id == b.id:
+# 					make_book = {
+# 						'book_id': b.id,
+# 						'title': b.title,
+# 						'isbn': b.isbn,
+# 						'synopsis': b.synopsis,
+# 						'date_borrowed': e.borrow_date
+# 					}
+# 					all_books.append(make_book)
+# 		return jsonify({"Books not yet returned": all_books})
+#
+# 	abort(404)
+
+# if user_borrowed_books is not None:
+# 	all_books = []
+# 	for book_borrowed in available_books:
+# 		for e in books_returned:
+# 			if e.id == book_borrowed.id:
+# 				make_book = {
+# 					'book_id': book_borrowed.id,
+# 					'title': book_borrowed.title,
+# 					'isbn': book_borrowed.isbn,
+# 					'synopsis': book_borrowed.synopsis,
+# 					'date_borrowed': e.borrow_date,
+# 					'date_returned': e.return_date
+# 				}
+# 				all_books.append(make_book)
+# 	return jsonify({"history of borrowed books": all_books})
