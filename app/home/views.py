f5eca6b2732ae42c68d961f7da18c7ab7e7c8d9c
Nerldy_ <arnodhis@gmail.com>
2018-05-11 10:28:59 +0300

    add pagination function

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,49 @@
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from ..models import BookList
 from . import home
+from cerberus import Validator
+
+pagination_schema = {
+	'limit': {
+		'type': 'string',
+		'required': True
+	},
+	'page_num': {
+		'type': 'string',
+		'required': True
+	}
+}
+
+validate_pagination_schema = Validator(pagination_schema)
+
+
+@home.route('/api/v1/books/results')
+def api_pagination():
+	req_args = request.args
+
+	if validate_pagination_schema.validate(req_args):
+		try:
+			page_limit_json = int(request.args.get('limit', None))  # check if page limit is provided
+			page_number = int(request.args.get('page_num', None))
+			book_pagination = BookList.query.paginate(per_page=int(page_limit_json), page=page_number, error_out=True)
+			book_results = []
+
+			for book in book_pagination.items:
+				book_obj = {
+					'id': book.id,
+					'title': book.title,
+					'isbn': book.isbn,
+					'synopsis': book.synopsis,
+				}
+
+				book_results.append(book_obj)
+
+			return jsonify({'current_page': book_pagination.page, 'all_pages': book_pagination.pages, 'books': book_results})
+
+		except:
+			return jsonify({"error": 'are all your queries integers? That or something else went wrong in your request'}), 400
+
+	return jsonify({"error": validate_pagination_schema.errors}), 400
 
 
 @home.route('/api/v1/books')
@@ -68,4 +111,3 @@ def api_get_book(id):
 	book_details['authors'] = book_authors
 
 	return jsonify({"book": book_details})
-
