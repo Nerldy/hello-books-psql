627549af062ca49570b304c8124ce7c7ce503bf2
Nerldy_ <arnodhis@gmail.com>
2018-05-12 11:02:19 +0300

    change config setting

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ login_manager = LoginManager()
 def create_app(config_name):
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_object(app_config[config_name])
-	app.config.from_pyfile('config.py')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	db.init_app(app)
 
